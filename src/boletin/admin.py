89e5ba1238b65e959251901e84d134c3830043d1
Fernando Gutierrez <fernandocarlos.gutierrez.gonzalez.alu@iesfernandoaguilar.es>
2022-04-21 11:49:19 +0200

    primera vista

--- src/boletin/admin.py
+++ src/boletin/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Registrado
 
-# Register your models here.
+
+
+class AdminRegistrado(admin.ModelAdmin):
+	list_display=["email","nombre","timestamp"]
+	list_filter= ["timestamp"]
+	list_editable=["nombre"]
+	search_fields=["email","nombre"]
+
+	class Meta:
+		model=Registrado
+
+
+
+admin.site.register(Registrado,AdminRegistrado)
