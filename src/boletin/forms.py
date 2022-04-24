d9a44d37188a24071e5efac736883fa43bb59c2e
Fernando Gutierrez <fernandocarlos.gutierrez.gonzalez.alu@iesfernandoaguilar.es>
2022-04-24 15:46:43 +0200

    contact custom

--- src/boletin/forms.py
+++ src/boletin/forms.py
@@ -6,7 +6,13 @@ class RegModelForm(forms.ModelForm):
 		model =  Registrado
 		fields = ["nombre","email"]
 
+	def clean_email(self):
+		email =  self.cleaned_data.get("email")
+		if not "edu" in email:
+			raise forms.ValidationError("Por favor introduza EDU como extension")
+		return "email@email.com"
 
-class RegForm(forms.Form):
-	nombre = forms.CharField(max_length=100)
+class ContactForm(forms.Form):
+	nombre = forms.CharField()
 	email = forms.EmailField()
+	mensaje = forms.CharField(widget=forms.Textarea)
--- src/boletin/views.py
+++ src/boletin/views.py
@@ -1,17 +1,45 @@
 from django.shortcuts import render
 
-from .forms import RegForm
+from .forms import ContactForm,RegModelForm
 
 from .models import Registrado
 
 def inicio(request):
-	form =  RegForm(request.POST or None)
-	if form.is_valid():
-		form_data = form.cleaned_data
-		abc = form_data.get("email")
-		abc2 = form_data.get("nombre")
-		obj = Registrado.objects.create(email=abc, nombre=abc2)
+	titulo = "HOLA"
+	if request.user.is_authenticated:
+		titulo = "Bienvenido %s" %(request.user)
+	form =  RegModelForm(request.POST or None)
 	context = {
-		"el_form": form,
+	"titulo": titulo,
+	"el_form": form,
 	}
+
+	if form.is_valid():
+		instance = form.save(commit=False)
+		nombre = form.cleaned_data.get("nombre")
+		email = form.cleaned_data.get("email")
+		if not instance.nombre:
+			instance.nombre = "Persona"
+		instance.save()
+
+		context = {
+		"titulo" : "Gracias %s!" %(nombre)
+		}
+		if not nombre:
+			context = {
+			"titulo" : "Gracias %s" %(email)
+			}
+		# form_data = form.cleaned_data
+		# abc = form_data.get("email")
+		# abc2 = form_data.get("nombre")
+		# obj = Registrado.objects.create(email=abc, nombre=abc2)
 	return render(request,"inicio.html",context)
+def contact(request):
+	form = ContactForm(request.POST or None)
+	if form.is_valid():
+		for key,value in form.cleaned_data.items():
+			print (key , value)
+	context = {
+	"form" : form,
+	}
+	return render(request, "forms.html",context)
